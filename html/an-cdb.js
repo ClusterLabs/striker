56da7c1cb9c5b798c8dbb55fe60a2759fccd9310
digimer <digimer@alteeve.ca>
2012-08-11 21:25:07 -0400

    Finished the initial code for scanning IPMI sensors. Fixed initial wave of bugs with the scanning of LSI controller and drive data. Increased the refresh icon size and added a back button to the left of the logo, removing the textual back buttons. Fixed some hash foreach loops.
    Signed-off-by: digimer <digimer@alteeve.ca>

--- html/an-cdb.js
+++ html/an-cdb.js
@@ -1,4 +1,10 @@
 function hide_on_load()
 {
+	document.getElementById('show_when_loaded').style.display = 'block'; 
+}
+
+function show_on_load()
+{
 	document.getElementById('hide_when_loaded').style.display = 'none'; 
 }
+
