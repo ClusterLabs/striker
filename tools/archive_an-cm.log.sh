875af9a051df6fbc4adf43a9356444d1bc8c3c7a
digimer <digimer@alteeve.ca>
2013-09-02 16:49:35 -0400

    Added two small scripts used to archive the megasas and an-cm log files to prevent them from getting too big.
    Signed-off-by: digimer <digimer@alteeve.ca>

---
+++ tools/archive_an-cm.log.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+# Delete the oldest archive
+if [ -e "/var/log/an-cm.log.6.bz2" ]
+then
+	rm -f /var/log/an-cm.log.6.bz2
+fi
+
+# Move 5 to 6
+if [ -e "/var/log/an-cm.log.5.bz2" ]
+then
+	mv /var/log/an-cm.log.5.bz2 /var/log/an-cm.log.6.bz2
+fi
+
+# Move 4 to 5
+if [ -e "/var/log/an-cm.log.4.bz2" ]
+then
+	mv /var/log/an-cm.log.4.bz2 /var/log/an-cm.log.5.bz2
+fi
+
+# Move 3 to 4
+if [ -e "/var/log/an-cm.log.3.bz2" ]
+then
+	mv /var/log/an-cm.log.3.bz2 /var/log/an-cm.log.4.bz2
+fi
+
+# Move 2 to 3
+if [ -e "/var/log/an-cm.log.2.bz2" ]
+then
+	mv /var/log/an-cm.log.2.bz2 /var/log/an-cm.log.3.bz2
+fi
+
+# Move 1 to 2
+if [ -e "/var/log/an-cm.log.1.bz2" ]
+then
+	mv /var/log/an-cm.log.1.bz2 /var/log/an-cm.log.2.bz2
+fi
+
+# Move current to 1 and compress it
+if [ -e "/var/log/an-cm.log" ]
+then
+	cp /var/log/an-cm.log /var/log/an-cm.log.1
+	cat /den/null > /var/log/an-cm.log
+	bzip2 /var/log/an-cm.log.1
+fi
---
+++ tools/archive_megasas.log.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# Delete the oldest archive
+if [ -e "/root/MegaSAS.log.5.bz2" ]
+then
+	rm -f /root/MegaSAS.log.5.bz2
+fi
+
+# Move 4 to 5
+if [ -e "/root/MegaSAS.log.4.bz2" ]
+then
+	mv /root/MegaSAS.log.4.bz2 /root/MegaSAS.log.5.bz2
+fi
+
+# Move 3 to 4
+if [ -e "/root/MegaSAS.log.3.bz2" ]
+then
+	mv /root/MegaSAS.log.3.bz2 /root/MegaSAS.log.4.bz2
+fi
+
+# Move 2 to 3
+if [ -e "/root/MegaSAS.log.2.bz2" ]
+then
+	mv /root/MegaSAS.log.2.bz2 /root/MegaSAS.log.3.bz2
+fi
+
+# Move 1 to 2
+if [ -e "/root/MegaSAS.log.1.bz2" ]
+then
+	mv /root/MegaSAS.log.1.bz2 /root/MegaSAS.log.2.bz2
+fi
+
+# Move current to 1 and compress it
+if [ -e "/root/MegaSAS.log" ]
+then
+	mv /root/MegaSAS.log /root/MegaSAS.log.1
+	bzip2 /root/MegaSAS.log.1
+fi
