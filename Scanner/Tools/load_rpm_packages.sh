f727581fae2b61e527f68a1655029007fc96ca23
Tom Legrady <Tom@TomLegrady.com>
2014-12-26 17:36:02 -0500

    Updated load_rpm_packages to install known rpms for Net::SNMP.

--- Scanner/Tools/load_rpm_packages.sh
+++ Scanner/Tools/load_rpm_packages.sh
@@ -2,13 +2,13 @@
 # 8 Modules from RPMs
 
 
-yum install ./perl-Sub-Exporter.noarch
-yum install ./perl-Clone.x86_64 
-yum install ./perl-TermReadKey.x86_64 
-yum install ./perl-DBI.x86_64
-yum install ./perl-List-MoreUtils.x86_64 
-yum install ./perl-XML-Simple.noarch
-yum install ./perl-Test-Output
+yum install perl-Sub-Exporter.noarch
+yum install perl-Clone.x86_64 
+yum install perl-TermReadKey.x86_64 
+yum install perl-DBI.x86_64
+yum install perl-List-MoreUtils.x86_64 
+yum install perl-XML-Simple.noarch
+yum install perl-Test-Output
 
 # ----------------------------------------------------------------------
 # Local modules
@@ -31,3 +31,13 @@ rpm -ivh ./perl-Proc-Background-1.10-1.el6.noarch.rpm
 rpm -ihv ./perl-Devel-GlobalDestruction-0.13-1.el6.noarch.rpm
 rpm -ihv ./perl-Class-Tiny-1.000-1.rhel6.noarch.rpm
 ----------------------------------------------------------------------
+# SNMP components 
+#
+yum install perl-Digest-SHA1.x86_64
+yum install perl-Digest-SHA.x86_64
+yum install perl-Digest-HMAC.noarch
+
+rpm -ihv perl-Crypt-CBC-2.33-1.el6.noarch.rpm
+rpm -ihv perl-Crypt-DES-2.07-1.el6.x86_64.rpm
+rpm -ihv perl-Crypt-DES-debuginfo-2.07-1.el6.x86_64.rpm
+rpm -ihv perl-Net-SNMP-v6.0.1-1.el6.noarch.rpm
