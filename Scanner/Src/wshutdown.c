c71e31a71ac882ddf496706a1f2a9c60c930f65d
Tom Legrady <tom@alteeve.ca>
2015-01-26 17:29:20 -0500

    completed SUID of wshutdown

--- Scanner/Src/wshutdown.c
+++ Scanner/Src/wshutdown.c
@@ -6,6 +6,10 @@
 int main ( ) {
 
   char * env[] = { "HOME=/root", "PATH=/sbin", (char *)0 };
+ 
+  setuid( geteuid() );
+
+  printf( "Path is %s\n", env[1] );
 
   int ret = execle( "/var/www/tools/safe-anvil-shutdown", "safe-anvil-shutdown",
                     (char *) 0, env );
