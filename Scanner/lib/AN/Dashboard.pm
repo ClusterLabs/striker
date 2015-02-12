090fba3d921195f321906554b9e23f404a351fb4
Tom Legrady <Tom@TomLegrady.com>
2015-02-11 19:48:27 -0500

    Documentation & POD

--- Scanner/lib/AN/Dashboard.pm
+++ Scanner/lib/AN/Dashboard.pm
@@ -6,6 +6,8 @@ use parent 'AN::Scanner';
 use warnings;
 use strict;
 use 5.010;
+use version;
+our $VERSION = '1.0.0';
 
 use Carp;
 use Const::Fast;
@@ -13,141 +15,196 @@ use Data::Dumper;
 use English qw( -no_match_vars );
 use File::Basename;
 
+# ======================================================================
+# CLASS ATTRIBUTES
+#
+
+use Class::Tiny qw(host ), { launched_monitor => sub { {}; }
+                           };
+
+# ======================================================================
+# CONSTANTS
+#
 const my $PROG => ( fileparse($PROGRAM_NAME) )[0];
 
-use Class::Tiny qw(host ), {launched_monitor => sub{ {}; }};
+const my $STD_NODE_SERVER_RECORD => [
+                                      {  message_tag => 'NODE_SERVER_STATUS',
+                                         status => [ 'DEAD', 'TIEMOUT', 'OK' ],
+                                      },
+                                      {  message_tag => 'AUTO_BOOT',
+                                         status      => [ 'TRUE', 'FALSE' ],
+                                      }, ];
 
-# DB sample record ...
-#  1 | 1 | node monitor | an-a07n01 | | node server | an-a07n01 | | TIMEOUT |
-#  NODE_SERVER_STATUS | host=an-a07n01 | 2015-01-29 21:39:31.774518-05
-#
+const my $MSG_BAD_NODE_SERVER_RECORD =>
+    "Something's wrong with records received for check_node_server_status\n"
+    . "\t-- Wrong message tag or status:\n"
+
+## Please see file perltidy.ERR
+    # ======================================================================
+    # Generate alert records for NODE_SERVER_STATUS for server 'DEAD' or
+    # 'OK', or TIMEOUT trying to restart the server.
+    #
+    sub set_server_generic_alert {
+    my $self = shift;
+    my ( $value, $status, $override ) = @_;
+
+    $override ||= '';    # printable version of 'false'
+    my $long_override = $override ? 'override existing alert' : '';
+
+    my @args = ( 0,                               # id
+                 $PID,                            # pid
+                 'NODE_SERVER_STATUS',            # field
+                 $value,                          # value
+                 '',                              # units
+                 $status,                         # status
+                 'NODE_SERVER_STATUS',            # message_tag
+                 "host=@{[$self->host]}",         # message_arguments
+                 AN::Unix::hostname('-short'),    # target_name
+                 'dashboard',                     # target_type
+                 $override,                       #target_extra
+                 $long_override, );
+
+    $self->set_alert(@args);
+    return;
+}
+
+# ......................................................................
 sub set_server_timeout_alert {
     my $self = shift;
 
-    my @args = ( 0,             # id
-                 $PID,          # pid
-                 'NODE_SERVER_STATUS', # field
-                 'TIMEOUT',            # value
-                 '',                   # units
-                 'CRISIS',             # status
-                 'NODE_SERVER_STATUS', # message_tag
-                 "host=@{[$self->host]}",     # message_arguments
-                 AN::Unix::hostname('-short'),# target_name
-                 'dashboard',                 # target_type
-                 'override',                  #target_extra
-                 'override existing alert'
-        );
-
-
-
-
-
-    $self->set_alert(@args)
+    $self->set_server_generic_alert( 'TIMEOUT', 'CRISIS', 'override' );
+    return;
 }
+
+# ......................................................................
 sub set_server_ok_alert {
     my $self = shift;
-    my ( $override ) = @_;
+    my ($override) = @_;
 
-    $override ||= '';
-    my $long_override = $override ? 'override existing alert' : ''; 
-    my @args = ( 0,             # id
-                 $PID,          # pid
-                 'NODE_SERVER_STATUS_OK', # field
-                 'OK',                    # value
-                 '',                      # units
-                 'OK',                    # status
-                 'NODE_SERVER_STATUS_OK', # message_tag
-                 "host=@{[$self->host]}",     # message_arguments
-                 AN::Unix::hostname('-short'),# target_name
-                 'dashboard',                 # target_type
-                 $override,                   #target_extra
-                 $long_override,
-        );
-
-    $self->set_alert(@args)
+    $self->set_server_generic_alert( 'OK', 'OK', $override );
+    return;
 }
+
+# ......................................................................
 sub set_server_dead_alert {
     my $self = shift;
 
-    my @args = ( 0,             # id
-                 $PID,          # pid
-                 'NODE_SERVER_STATUS', # field
-                 'DEAD',               # value
-                 '',                   # units
-                 'CRISIS',             # status
-                 'NODE_SERVER_STATUS', # message_tag
-                 "host=@{[$self->host]}", # message_arguments
-                 AN::Unix::hostname('-short'),# target_name
-                 'dashboard',          # target_type
-                 '',                   #target_extra
-        );
-
-
-
-
-
-    $self->set_alert(@args)
+    $self->set_server_generic_alert( 'DEAD', 'CRISIS' );
+    return;
 }
 
+# ......................................................................
+# Utility - Is the record a NODE_SERVER_STATUS record?
+#
+sub isa_node_server_record {
+    my $self = shift;
+    my ($tag) = @_;
+
+    return $record->{message_tag} eq 'NODE_SERVER_STATUS';
+}
+
+# ......................................................................
+#  Utility - Is the record a AUTO_BOOT record?
+#
+sub isa_auto_boot_record {
+    my $self = shift;
+    my ($tag) = @_;
+
+    return $record->{message_tag} eq 'AUTO_BOOT';
+}
+
+# ......................................................................
+# Utility - Map status to string for downstream processing of node
+# server record.
+#
+sub map_node_server_status2string {
+    my $self = shift;
+    my ($status) = @_;
+
+    return
+          $status eq 'DEAD'    ? 'dead'
+        : $status eq 'TIMEOUT' ? 'timeout'
+        : $status eq 'OK'      ? 'alive'
+        :                        undef;
+}
+
+# ......................................................................
+# Utility - Map status to string for downstream processing of auto
+# boot record.
+#
+sub map_auto_boot_status2string {
+    my $self = shift;
+    my ($status) = @_;
+
+    return
+          $status eq 'FALSE' ? 'disabled by user'
+        : $status eq 'TRUE'  ? 'should be running'
+        :                      undef;
+}
+
+# ----------------------------------------------------------------------
+# Determine whether the server is running. Query the databases for
+# records announcing the failure of a server, or disabling/enabling
+# autboot.
+#
 sub parse_node_server_status {
     my $self = shift;
 
-    my $records = $self->dbs()->check_node_server_status($self->host);
+    my $records = $self->dbs()->check_node_server_status( $self->host );
+
     my ( $dead_or_alive, $autoboot, $aok );
     for my $record (@$records) {
-        if ( $record->{message_tag} eq 'NODE_SERVER_STATUS' ) {
+        if ( $self->isa_node_server_record( $record->{message_tag} ) ) {
             $dead_or_alive
-                = $record->{status} eq 'DEAD'    ? 'dead'
-                : $record->{status} eq 'TIMEOUT' ? 'timeout'
-                : $record->{status} eq 'OK'      ? 'alive'
-                :                                  undef;
+                = $self->map_node_server_status2string( $record->{status} );
             $aok = defined $dead_or_alive;
         }
-        elsif ( $record->{message_tag} eq 'AUTO_BOOT' ) {
-            $autoboot
-                = $record->{status} eq 'FALSE' ? 'disabled by user'
-                : $record->{status} eq 'TRUE'  ? 'should be running'
-                :                                undef;
-            $aok = defined $autoboot;
+        elsif ( $self->isa_auto_boot_record( $record->{message_tag} ) ) {
+            $autoboot = $self->map_auto_boot_status2string( $record->{status} );
+            $aok      = defined $autoboot;
         }
 
-        carp( "Something's wrong with records received for ",
-              " check_node_server_status--Wrong message tag or status\n",
+        carp( $MSG_BAD_NODE_SERVER_RECORD,
               Dumper( [
-                          { should_be => [
-                                { message_tag => 'NODE_SERVER_STATUS',
-                                  status      => [ 'DEAD', 'TIEMOUT', 'OK' ],
-                                },
-                                { message_tag => 'AUTO_BOOT',
-                                  status      => [ 'TRUE', 'FALSE' ],
-                                },
-                                ],
-                             actual_record => $record,
-                          },
+                         {  should_be     => $STD_NODE_SERVER_RECORD,
+                            actual_record => $record,
+                         },
                       ],
-              ), )
+                    ) )
             unless $aok;
     }
     return ( $dead_or_alive, $autoboot );
 }
 
+# ----------------------------------------------------------------------
+# Determine whether node servers are running. If a server IS running,
+# as the result of launching a node_monitor, halt the node_monitor,
+# and set an alert to notify the return of the server.
+#
+# If 'autoboot' has been turned off, ignore servers that are not running.
+#
+# If server is dead, launch a node_monitor agent and set a 'DEAD'
+# alert. If node_monitor has timed out waiting for server to restart,
+# set a 'TIMEOUT' alert.
+#
 sub check_node_server_status {
     my $self = shift;
 
     state $ns_keys = [ keys %{ $self->confdata()->{node_server} } ];
-  KEY:
+KEY:
     for my $key (@$ns_keys) {
         $self->host( $self->confdata()->{node_server}{$key} );
+
         my ( $dead_or_alive, $autoboot ) = $self->parse_node_server_status;
 
-        if ( $dead_or_alive  # node server on host is running OK
+        if (    $dead_or_alive
              && $dead_or_alive eq 'alive' ) {
-            if ( $self->launched_monitor->{$self->host} ) {
-                $self->set_server_ok_alert( 'override' );
-                delete $self->launched_monitor->{$self->host};
+            if ( $self->launched_monitor->{ $self->host } ) {
+                $self->set_server_ok_alert('override');
+                delete $self->launched_monitor->{ $self->host };
             }
             next KEY;
-        } 
+        }
 
         next KEY
             if $autoboot
@@ -157,13 +214,16 @@ sub check_node_server_status {
             $self->set_server_timeout_alert;
         }
         elsif ( $dead_or_alive eq 'dead' ) {
+
             # Not running, but it should be.
-            $self->launched_monitor->{$self->host} = 1;
+            $self->launched_monitor->{ $self->host } = 1;
             $self->set_server_dead_alert;
             my $agent = $self->confdata->{node_server_down_agent};
             my $args = { ignore_ignorefile => { $agent => 1 },
-                         args => [-host       => $self->host,
-                                  -healthfile => $self->confdata()->{healthfile}],};
+                         args              => [
+                                  -host       => $self->host,
+                                  -healthfile => $self->confdata()->{healthfile}
+                                 ], };
             $self->launch_new_agents( [$agent], $args );
         }
         else {
@@ -173,46 +233,16 @@ sub check_node_server_status {
     return;
 }
 
-# The dashboard version of process_agent_data() does not pass on
-# ordinary alerts to be display dispatchers.
-sub process_agent_data {
-    my $self = shift;
+# ----------------------------------------------------------------------
+# ignore individial alerts, in dashboard; use only the weights.
+#
+sub detect_status { }
 
-    state $dump = grep {/dump alerts/} $ENV{VERBOSE} || '';
-    say "${PROG}::process_agent_data()." if $self->verbose;
-
-PROCESS:
-    for my $process ( @{ $self->processes } ) {
-        my $alerts = $self->fetch_alert_data($process);
-
-        next PROCESS
-            unless 'ARRAY' eq ref $alerts
-            && @$alerts;
-
-        my $allN = scalar @$alerts;
-        my $newN = 0;
-        say Data::Dumper::Dumper( [$alerts] )
-            if $dump;
-        my $seen_summary = 0;
-    ALERT:
-        for my $alert (@$alerts) {
-            if ( $alert->{field} eq 'summary' ) {
-                last ALERT
-                    if $seen_summary++;    # this is from an earlier loop
-
-                $self->sumweight( $self->sumweight + $alert->{value} );
-            }
-            $newN++;
-        }
-        say scalar localtime(), " Received $allN alerts for process ",
-            "$process->{name}, $newN of them new; weight is @{[$self->sumweight]}."
-            if $self->verbose || grep {/\balertcount\b/} $ENV{VERBOSE} || '';
-    }
-
-    return;
-}
-
-# Things to do in the core for a $PROG core object
+# ----------------------------------------------------------------------
+# Things to do in the core for a $PROG core object. Besides the usual
+# tasks of launching agents and processing the alerts they generate,
+# check_node_server_status() verifies the listed node servers are
+# running and re-starts them if they are not.
 #
 sub loop_core {
     my $self = shift;
@@ -243,26 +273,28 @@ __END__
 
 =head1 NAME
 
-     Scanner.pm - System monitoring loop
+     Dashboard.pm - System monitoring loop for dashboard platforms
 
 =head1 VERSION
 
-This document describes Scanner.pm version 0.0.1
+This document describes Dashboard.pm version 1.0.0
 
 =head1 SYNOPSIS
 
-    use AN::Scanner;
-    my $scanner = AN::Scanner->new();
-
+    use AN::Dashboard;
+    my $dashboard = AN::Dashboard->new();
+    $dashboard->run();
 
 =head1 DESCRIPTION
 
-This module provides the Scanner program implementation. It monitors a
-HA system to ensure the system is working properly.
+This module provides the Dashboard program implementation. It monitors
+a HA system to ensure the servers are running, and restarts them if
+they are down.
 
 =head1 METHODS
 
-An object of this class represents a scanner object.
+An object of this class represents a dashboard object. Most behaviour is
+inherited from the AN::Scanner parent class.
 
 =over 4
 
@@ -281,8 +313,52 @@ The directory that is scanned for scanning plug-ins.
 
 How often the loop should scan.
 
+=item B<run_until>
+
+hh::mm::ss defining a stop time. Cron jobs verify the program is
+running, so halting the program just before one of the cron checks
+will replace the instance with a new one, once a day.
+
+=item B<dbconf>
+
+The full path to the file defining the databases to access.
+
+=item B<confpath>
+
+The full path to the file configuring this program.
+
+=item B<msg_dir>
+
+The path to the directory containing the message file. THe message
+file for each program is generated by combining the name of the
+program with a C".xml" suffix.
+
+=item B<from>
+=item B<smtp>
+
+The email sender and SMTP interface host.
+
+=item B<max_retries>
+
+How many times AN::Common should attempt to look up a string, before giving up.
+
+=item B<bindir>
+
+Directory where system-interface binary programs are located.
+
+=item B<logdir>
+
+Directory where marker files and log files are stored.
+
 =back
 
+Additionally, the C<log> element may be set to C<1> if the program
+should keep a log file. The C<commandlineargs> element can store the
+command line arguments which would relaunch the program with the same
+configuration. If databases come and go, rather than trying
+complicated processing to maintain integrity, the program relaunches
+itself, and integral database connections are achieved during
+initialization.
 
 =back
 
@@ -290,25 +366,29 @@ How often the loop should scan.
 
 =over 4
 
+=item B<Carp> I<core>
+
+Report errors as if they occur at call site.
+
+=item B<Class::Tiny>
+
+A simple OO framework. "Boilerplate is the root of all evil"
+
+=item B<Const::Fast>
+
+Provides fast constants.
+
 =item B<English> I<core>
 
 Provides meaningful names for Perl 'punctuation' variables.
-
-=item B<version> I<core since 5.9.0>
-
-Parses version strings.
 
 =item B<File::Basename> I<core>
 
 Parses paths and file suffixes.
 
-=item B<FileHandle> I<code>
+=item B<version> I<core since 5.9.0>
 
-Provides access to FileHandle / IO::* attributes.
-
-=item B<FindBin> I<core>
-
-Determine which directory contains the current program.
+Parses version strings.
 
 =back
 
@@ -353,7 +433,7 @@ There are no current incompatabilities.
 
 Alteeve's Niche!  -  https://alteeve.ca
 
-Tom Legrady       -  tom@alteeve.ca	November 2014
+Tom Legrady       -  tom@alteeve.ca	January 2015
 
 =cut
 
