7ff521d66c8034d29a98af7d927df16d01644a54
Tom Legrady <Tom@TomLegrady.com>
2015-02-28 16:38:25 -0500

    feature (system_check) system_check testing PDU shutdown of servers and switches.

--- Scanner/lib/AN/SystemCheck.pm
+++ Scanner/lib/AN/SystemCheck.pm
@@ -15,6 +15,7 @@ use English '-no_match_vars';
 use File::Basename;
 use File::Spec::Functions 'catdir';
 use File::Temp 'tempfile';
+use Net::SNMP;
 use POSIX 'strftime';
 use Time::HiRes qw( time alarm sleep);
 use Time::Local;
@@ -34,21 +35,27 @@ use AN::Unix;
 const my $PROG => ( fileparse($PROGRAM_NAME) )[0];
 
 use Class::Tiny qw( check index reference ssh ipmi names root tmpfiles
-                    cluster_conf etc_hosts
+                    cluster_conf etc_hosts 
                   ), {tmpdir => sub {'/var/tmp/systemcheck'},
+		      snmp   => sub { {} },
 };
 
 
 # ======================================================================
 # CONSTANTS
 #
+const my $BOUNCE => 3;
+
+const my $QUICK_DROP => '-o ServerAliveInterval=2';
 
 const my $COLON => q{:};
 const my $COMMA => q{,};
 const my $COMMASPACE => q{, };
+const my $DOT   => q{.};
 const my $SPACE => q{ };
 const my $STAR  => q{*};
 const my $PIPE  => q{|};
+const my $COLON_SEPARATOR => "\n" . ':' x 72 . "\n";
 
 const my $READ_PROC  => q{-|};
 const my $WRITE_PROC => q{|-};
@@ -62,7 +69,7 @@ const my $CMD
 	HOSTNAME         =>  '"uname -n"',
 	JOIN_CLUSTER     => '"/etc/init.d/cman start && /etc/init.d/rgmanager start"',
 	PANIC_CRASH_HOST => '"echo c > /proc/sysrq-trigger"',
-	TAIL_MSGS_FILE   => 'ssh -o ServerAliveInterval=2 host "tail -f /var/log/messages"',
+	TAIL_MSGS_FILE   => 'ssh $QUICK_DROP host "tail -f /var/log/messages"',
 	# --------------------------------------------------
 	# PDU test commands.
 	#
@@ -72,6 +79,33 @@ const my $CMD
 const my $FILE => { CRASHLOG =>   'crashlog_IP_XXXX',
 		    MSGTAILLOG => 'msgtaillog_IP_XXXX',
 };
+
+const my $MSG
+    => { FENCED => "%s /var/log/messages reports %s fenced.\n%s\n"
+	     .  "Waiting for reboot, at %s\n",
+         FENCED_TIMED_OUT
+	 => "Timed out (5 min) waiting for %s to detect %s crash.\n"
+	 .  "%s\nShutting down.\n",
+	 HAS_CRASHED => "Detected system crash:\n%s\n",
+	 PINGING => "Pinging %s -> %s\n",
+	 CRASHING => "Crashing host %s at %s\n",
+	 REPINGABLE => "Host %s is pingable again, at %s\n"
+	        .   "Waiting 15 seconds, then restarting cman & rgmanager.\n",
+	 PINGABLE_TIMED_OUT
+	 => "Timed out (10 min) waiting for %s to become pingable.\n"
+	 .   "shutting down",
+	 NO_CMAN_RGMANAGER => "Host %s not running cman / rgmanager '%s'.\n",
+	 WAITING => "Waiting 15 seconds for rcman and rgmanager to settle in",
+	 NO_ETC_HOSTS => "Failed to read /etc/hosts file: %s.\n",
+	 PDU_OFF => "Turning off PDU for %s:\n\t'%s'.\n",
+	 NO_IP_FOR_NAME => "Failed to look up IP for name '%s' in cached /etc/hosts\n",
+	 SNMP_CONNECT_FAILED => "Failed SNMP connect to host %s/%s: %s.\n",
+
+};
+const my $MONTHNAME2NUM => { Jan => 0, Feb => 1, Mar => 2, Apr => 3,
+			     May => 4, Jun => 5, Jul => 6, Aug => 7,
+			     Sep => 8, Oct => 9, Nov => 10, Dec => 11 };
+
 # ======================================================================
 # GLOBAL
 #
@@ -89,29 +123,29 @@ sub prepare_for_tests {
     $self->ssh( [ split $COMMA, $self->confdata->{servers}{ssh} ]);
     $self->ipmi( [ split $COMMA, $self->confdata->{servers}{ipmi} ]);
 
-    mkdir $self->tmpdir
-	unless -d $self->tmpdir;
+    mkdir $self->tmpdir()
+	unless -d $self->tmpdir();
 
 }
 # ----------------------------------------------------------------------
 #
 sub generic_ssh_test {
     my $self = shift;
-    my ( $hosts, $remote_cmd ) = @_;
+    my ( $hosts, $remote_cmd, $option ) = @_;
 
+    $option ||= '';
     my (%results, @failed);
     for my $host ( @$hosts ) {
-     	my @cmd = ( '/usr/bin/ssh', $host, $remote_cmd );
+     	my @cmd = ( '/usr/bin/ssh', $option, $host, $remote_cmd );
      	say "Running '@cmd'" if $self->verbose;
      	my $response = `@cmd`;
      	chomp $response;
-     	say "Got response '$response'" if $self->verbose;
+     	say "Got response '$response'";
      	if ( length $response ) {
      	    $results{$host} = $response;
      	} else {
      	    push @failed, $host;
      	}
-     	say '' if $self->verbose;;
     }
     return (\%results, \@failed );;
 }
@@ -153,7 +187,6 @@ sub launch_msg_tail_on_host {
     my ( $host ) = @_;
 
     my $logfile = $self->logdir . '/messages.' . $host;
-#    my $logfile = $self->logdir . '/tmp/messages.' . $host;
     my $cmd = '(/usr/bin/ssh root@'
 	. $host
 	. ' "tail -f /var/log/messages" ) > '
@@ -168,7 +201,7 @@ sub crash_host {
     my $self = shift;
     my ( $host ) = @_;
 
-    my $cmd = qq{ssh  -o ServerAliveInterval=2 root\@$host "echo c > /proc/sysrq-trigger" };
+    my $cmd = qq{ssh $QUICK_DROP root\@$host "echo c > /proc/sysrq-trigger" };
     say "Running '$cmd'" if $self->verbose;
     system( $cmd );
 
@@ -176,22 +209,48 @@ sub crash_host {
 }
 # ----------------------------------------------------------------------
 #
+sub fetch_log_lines_since_crash {
+    my $self = shift;
+    my ( $since, $logfile ) = @_;
+
+    $since -= $since % 60;	# discard seconds.
+    my @lines = split "\n", `cat $logfile`;;
+    my @newlines;
+    my $year = 1900 + (localtime)[5];
+  LINE:
+    for my $line ( @lines ) {
+	my @datetime = split $SPACE, $line, 4;
+	my $month = $MONTHNAME2NUM->{$datetime[0]};
+	my ( $hours, $minutes ) = split $COLON, $datetime[2];
+	my $time = timelocal( 0, $minutes, $hours, $datetime[1], $month, $year);
+	next LINE if $time < $since;
+	push @newlines, $line;
+    }
+    return @newlines;
+}
+# ----------------------------------------------------------------------
+#
 sub check_log_file_for_fenced_host {
     my $self = shift;
-    my ( $logfile ) = @_;
+    my ( $host, $logfile ) = @_;
 
     my $crashed_at = time;
     my (@lines, $has_crashed, $is_fenced);
 
   LINE:
     while ( time() - $crashed_at < 300 ) {
-	@lines = split "\n", `cat $logfile`;
-	$has_crashed = grep {/Connection closed/} @lines;
+	@lines = $self->fetch_log_lines_since_crash( $crashed_at, $logfile );
+	$has_crashed
+	    = grep { /drbd\d:\sPingAck \s did \s not \s arrive \s in \s time./xms
+	           } @lines;
+	printf $MSG->{HAS_CRASHED}, $has_crashed
+	    if $has_crashed;
 
-	$is_fenced = grep { /rgmanager/ && /Restricted domain unavailable/
-	                  } @lines;
+	$is_fenced = grep { /(?:fence \s $host \s success)
+                          || (?:peer \s was \s stonithed)/xms} @lines;
 	last LINE if $is_fenced;
     }
+
     return ($has_crashed, $is_fenced, @lines );
 }
 # ----------------------------------------------------------------------
@@ -201,14 +260,14 @@ sub wait_for_pingable {
     my ( $host ) = @_;
 
     my ( $start, $host_is_pingable, $i ) = (time(), 0, 0);
-    say "Pinging $host -> ", scalar localtime;
+    printf $MSG->{PINGING}, $host, scalar localtime;
   PING:
     while ( time() - $start < 600 ) {
 	$host_is_pingable = $self->ping_host_once($host);
 	last PING if $host_is_pingable;
 	sleep 10;
 	$i++;
-	say "Pinging $host -> ", scalar localtime
+	printf $MSG->{PINGING}, $host, scalar localtime
 	    if 0 == $i % 4;	# approx 1 minute, including ping timeout
     }
     return $host_is_pingable;
@@ -228,11 +287,56 @@ sub ping_host_once {
 }
 # ----------------------------------------------------------------------
 #
+sub join_cluster {
+    my $self = shift;
+    my ( $host ) = @_;
+
+    my ( $results, $failed )
+	= $self->generic_ssh_test( [$host], $CMD->{JOIN_CLUSTER});
+
+    die sprintf $MSG->{NO_CMAN_RGMANAGER}, $host, join $SPACE, @$failed
+	if 'ARRAY' eq ref $failed
+	&& @$failed;
+
+    return;
+}
+# ----------------------------------------------------------------------
+#
+sub monitor_host_recovery {
+    my $self = shift;
+    my ( $host, $otherhost, $bg_task, $logfile ) = @_;
+
+    my ( $has_crashed, $is_fenced, @lines )
+	= $self->check_log_file_for_fenced_host( $host, $logfile );
+
+    if ( $is_fenced ) {
+	printf( $MSG->{FENCED}, $otherhost, $host,
+		join( "\n", @lines), scalar localtime);
+	$bg_task->{process}->die();
+    } else {
+	printf( $MSG->{FENCED_TIMED_OUT}, $otherhost, $host,
+		join( "\n", @lines));
+	exit 1;
+    }
+    my ( $host_is_back ) = $self->wait_for_pingable( $host );
+    if ( $host_is_back ) {
+	printf $MSG->{REPINGABLE}, $host, scalar localtime;
+    } else {
+	die sprintf $MSG->{PINGABLE_TIMED_OUT}, $host;
+	exit 1;
+    }
+    sleep 15;
+    $self->join_cluster( $host );
+
+    return;
+}
+# ----------------------------------------------------------------------
+#
 sub test_panic_crash {
     my $self = shift;
 
-    my ($i, $N) = (1, scalar @{$self->ssh} )
-;
+    my ($i, $N) = (1, scalar @{$self->ssh} );
+
     for my $host ( @{$self->ssh} ) {
 	# monitor /var/log/messages on other host
 	#
@@ -241,45 +345,18 @@ sub test_panic_crash {
 
 	# crash the selected hosts
 	#
-	say "Crashing host $host at ", scalar localtime;
+	printf $MSG->{CRASHING}, $host, scalar localtime;
 	$self->crash_host( $host );
 	say "Job done at ", scalar localtime;
 
-	my ( $has_crashed, $is_fenced, @lines )
-	    = $self->check_log_file_for_fenced_host( $logfile );
-
-	if ( $is_fenced ) {
-	    say "$otherhost /var/log/messages reports $host fenced.";
-	    say "\n", join( "\n", @lines), "\n";
-	    say "Waiting for reboot, at ", scalar localtime;
-	    $bg_task = undef;   # kill bg task
-	} else {
-	    say "Timed out (5 min) waiting for $otherhost to detect $host crash.";
-	    say "\n", join( "\n", @lines), "\n";
-	    say "shutting down";
-	    exit 1;
-	}
-	my ( $host_is_back ) = $self->wait_for_pingable( $host );
-	if ( $host_is_back ) {
-	    say "Host $host is pingable again, at ", scalar localtime;
-	    say "Waiting 15 seconds, then restarting cman & rgmanager."
-	} else {
-	    die "Timed out (10 min) waiting for $host to become pingable.";
-	    say "shutting down";
-	    exit 1;
-	}
-	sleep 15;
-	my ( $results, $failed )
-	    = $self->generic_ssh_test( [$host], $CMD->{JOIN_CLUSTER});
-
-	die "Host $host not running cman / rgmanager '@$failed'.\n"
-	    if 'ARRAY' eq ref $failed
-	    && @$failed;
+	# detect results on other host.
+	#
+	$self->monitor_host_recovery( $host, $otherhost, $bg_task, $logfile );
 
 	if ( $i++ < $N ) {	# Don't bother on last time around.
-	    say "Waiting 15 seconds for rcman and rgmanager to settle in";
+	    say $MSG->{WAITING};
 	    sleep 15;
-	    say "\n", ':' x 72, "\n";
+	    say $COLON_SEPARATOR;
 	}
     }
     return;
@@ -344,7 +421,7 @@ sub load_etc_hosts {
     my ( $results, $failed )
 	= $self->generic_ssh_test( $self->ssh, $CMD->{GET_ETC_HOSTS});
 
-    die "Failed to read /etc/hosts file: @$failed."
+    die sprintf $MSG->{NO_ETC_HOSTS}, join $SPACE, @$failed
 	if @$failed;
 
     my $lines = $results->{$self->ssh->[0]};
@@ -374,6 +451,190 @@ sub load_etc_hosts {
     return;
 }
 # ----------------------------------------------------------------------
+#
+sub turn_both_outlets_off {
+    my $self = shift;
+    my ($cmds, $name) = @_;
+    
+    for my $idx ( sort keys %$cmds ) {
+	my $cmd = $cmds->{$idx}->{command};
+	my ($pdu) = ( $cmd =~ m{-a \s (\S+) \s -n}xms );
+	my $ip = $self->etc_hosts->{$pdu};
+	$cmd =~ s{$pdu}{$ip};
+	$cmd .= ' -o off';
+	printf $MSG->{PDU_OFF}, $name, $cmd;
+	my $response = `$cmd`;
+	say "Got response '$response'";
+    }
+    return;
+}
+# ----------------------------------------------------------------------
+#
+sub run_server_pdu_tests {
+    my $self = shift;
+
+    my ($i, $N) = (1, scalar @{$self->ssh} );
+
+    for my $host ( @{$self->ssh} ) {
+	my $name = $self->names->{$host};
+	my $cmds = $self->cluster_conf->{node}{$name}{fence_method}{'1:pdu'}{device};
+
+	# monitor /var/log/messages on other host
+	#
+	my ($otherhost) = grep { $_ ne $host } @{$self->ssh};
+	my ($bg_task, $logfile) = $self->launch_msg_tail_on_host( $otherhost );
+
+	$self->turn_both_outlets_off( $cmds, $name );
+
+	$self->monitor_host_recovery( $host, $otherhost, $bg_task, $logfile );
+
+	say $MSG->{WAITING};
+	sleep 15;
+	say $COLON_SEPARATOR;
+    }
+    return;
+}
+# ----------------------------------------------------------------------
+#
+sub parse_and_set_reboot_delay {
+    my $self = shift;
+    my ( $session, $response, $delay ) = @_;
+
+    local $LIST_SEPARATOR = $COMMASPACE;
+
+    my @set;
+    my $intro = "Setting reboot delay to $delay using SNMP data: \n";
+  OID:
+    for my $oid ( sort keys %$response ) {
+	next OID if $response->{$oid} == $delay;
+	say $intro if $intro;
+	$intro = undef;
+	my @args = ($oid, INTEGER, $delay);
+	say "\t@args";
+	my $result = $session->set_request( -varbindlist => \@args);
+	say $result
+	    ? "\t\tModification succeeded."
+	    : "\t\tModification failed: $result";	
+    }
+    say "Reboot delays are already $delay, no action required."
+	if $intro;
+
+    return
+}
+# ----------------------------------------------------------------------
+#
+sub connect_snmp {
+    my $self = shift;
+    my ( $config, $pw, $unit ) = @_;
+
+    if ( ! exists $self->snmp()->{$unit} ) {
+	my $name = $config->{$unit}{pdu};
+	say "Processing PSU $unit - $name.";
+	my $ip = $self->etc_hosts->{$name};
+	die sprintf $MSG->{NO_IP_FOR_NAME}, $name
+	    unless defined $ip;
+	my $outlet = $config->{$unit}{outlet};
+
+	my ( $session, $error )
+	    = Net::SNMP->session( -hostname     => $ip,
+				  -community    => 'public',
+				  -version      => 'snmpv2c', );
+
+	die sprintf $MSG->{SNMP_CONNECT_FAILED}, $name, $ip, $error
+	    unless defined $session;
+
+	$self->snmp()->{$unit} = $session;
+    }
+    return $self->snmp()->{$unit};
+}
+# ----------------------------------------------------------------------
+#
+sub set_delay_on_both_units {
+    my $self = shift;
+    my ( $config, $pw, $oids ) = @_;
+
+    for my $unit ( qw( psu1 psu2 )) {
+	my $session = $self->connect_snmp( $config, $pw, $unit );
+
+	my $response = $session->get_request( -varbindlist
+					      => $oids->{reboot_delay} );
+
+	$self->parse_and_set_reboot_delay( $session, $response, 30 );
+    }
+    return;
+}
+# ----------------------------------------------------------------------
+#
+sub bounce_both_outlets {
+    my $self = shift;
+    my ( $config, $pw, $oids ) = @_;
+
+    my $intro = "Reboot outlet using SNMP data: \n";
+    
+    for my $unit ( qw( psu1 psu2 )) {
+	my $session = $self->connect_snmp( $config, $pw, $unit );
+	my $outlet = $config->{$unit}{outlet};
+	my $oid = $oids->{outlet_status}[$outlet - 1];
+	my @args = ($oid, INTEGER, $BOUNCE);
+	say "\t@args";
+	my $result = $session->set_request( -varbindlist => \@args);
+	say $result
+	    ? "\t\tModification succeeded: $result->{$oid}."
+	    : "\t\tModification failed: $result";	
+    }
+    return;
+}
+# ----------------------------------------------------------------------
+#
+sub monitor_proc_net_bonding {
+    my $self = shift;
+
+    my $cmd = 'cat /proc/net/bonding/bcn-bond1';
+    my $start = time();
+    while ( time() - $start < 300 ) {
+	say "\n", '-' x 72, "\n", scalar localtime;
+	my ($results, $failed)
+	    = $self->generic_ssh_test( [$self->ssh->[0]], $cmd, $QUICK_DROP );
+	sleep 15;
+    }
+    say "Stop logging at ", scalar localtime;
+    say "\n", '-' x 72, "\n";
+    return;
+}
+# ----------------------------------------------------------------------
+#
+sub snmp_both_outlets_off {
+    my $self = shift;
+    my ( $config, $pw, $oids ) = @_;
+
+
+    $self->set_delay_on_both_units( $config, $pw, $oids );
+   $self->bounce_both_outlets( $config, $pw, $oids ); # working
+    $self->monitor_proc_net_bonding( );
+    return;
+}
+# ----------------------------------------------------------------------
+#
+sub run_network_switch_pdu_tests {
+    my $self = shift;
+
+    my $name = $self->names->{$self->ssh->[0]};
+    my $cmds = $self->cluster_conf->{node}{$name}{fence_method}{'1:pdu'}{device};
+    my $pw   = $self->confdata->{switch}{pw};
+    my $outlet_status = $self->confdata->{switch}{oid}{outlet_status};
+    my $reboot_delay  = $self->confdata->{switch}{oid}{reboot_delay};
+    my $oids = { reboot_delay
+		     => [ map { $reboot_delay . $DOT . $_ } ( 1..8 ) ],
+		 outlet_status
+		     => [ map { $outlet_status . $DOT . $_ } ( 1..8 ) ],
+    };
+  SWITCH:
+    for my $switch ( grep {/\d+/} keys %{ $self->confdata->{switch} } ) {
+	last SWITCH if $switch != 1;
+	$self->snmp_both_outlets_off( $self->confdata->{switch}{$switch}, $pw, $oids);
+    }
+}
+# ----------------------------------------------------------------------
 # 
 sub run_pdu_tests {
     my $self = shift;
@@ -385,18 +646,9 @@ sub run_pdu_tests {
     $self->load_cluster_conf_file;
     $self->load_etc_hosts;
     $self->separator;
-    for my $host ( @{$self->ssh} ) {
-	my $name = $self->names->{$host};
-	my @cmd = $self->cluster_conf->{node}{$name}{fence_method}{'1:pdu'}{device};
-	for my $cmd ( @cmds ) {
-	    my $pdu= ( $cmd =~ m{-a \s (\S+) \s -n}xms );
-	    my $ip = $self->etc_hosts->{$pdu};
-	    $cmd =~ s{$pdu}{$ip};
-	    say "Turning off PDU for $name: '$cmd'."
-		if $self->verbose;
-	}
-	say "hello";
-    }
+
+#    $self->run_server_pdu_tests; # working
+    $self->run_network_switch_pdu_tests;
     say "PDU tests complete.";
     return;
 }
@@ -416,7 +668,7 @@ sub run {
 #    $self->handle_changes($changes) if $changes;
 
     $self->prepare_for_tests();
-#    $self->run_cluster_tests();
+#    $self->run_cluster_tests(); # working
     $self->run_pdu_tests();
 
     $self->finalize_node_table_status();
