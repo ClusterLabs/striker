8b2f81385d8403fa854cb490eafff6b97dc396dc
Tom Legrady <Tom@TomLegrady.com>
2015-03-04 11:26:15 -0500

    docs(bonding agent): tidied code and added documentation

--- Scanner/lib/AN/Bonding.pm
+++ Scanner/lib/AN/Bonding.pm
@@ -18,15 +18,14 @@ use Const::Fast;
 # ======================================================================
 # CLASS ATTRIBUTES & CONSTRUCTOR
 
-use Class::Tiny qw( ), {prev => sub { {} },
-			hw_addr => sub { {} },
-                       } ;
+use Class::Tiny qw( ),
+    { prev    => sub { {} },
+      hw_addr => sub { {} }, };
 
 sub BUILD {
     my $self = shift;
 
     return unless ref $self eq __PACKAGE__;
-    
 
     return;
 }
@@ -39,20 +38,20 @@ const my $SLASH => q{/};
 const my $SPACE => q{ };
 
 const my $TAG => { current_slave => 'Currently Active Slave',
-		   combined_mii  => 'Combined MII Status',
-		   single_mii    => 'Single MII_Status',
-		   speed         => 'Single Side Speed',
-		   duplex        => 'duplex',
-		   hw_address    => 'permament hw address'
-                 };
+                   combined_mii  => 'Combined MII Status',
+                   single_mii    => 'Single MII_Status',
+                   speed         => 'Single Side Speed',
+                   duplex        => 'duplex',
+                   hw_address    => 'permament hw address' };
+
 # ======================================================================
 # METHODS
 #
 
 # ----------------------------------------------------------------------
-# Compare variables with specific values, such as 'battery replace',
-# which may be 'needed' or 'unneeded'. This routine is specific to
-# known agents.
+# Compare variables with specific values, to determine which slave
+# link is the primary, what speed the links are providing, and whether
+# they are full- or half-duplex.
 #
 sub eval_discrete_status {
     my $self = shift;
@@ -62,32 +61,29 @@ sub eval_discrete_status {
         = { args => '', tag => '', label => '', newval => '', status => '' };
 
     if ( $args->{tag} eq $TAG->{current_slave} ) {
-	$msg->{status} = 'OK';
+        $msg->{status} = 'OK';
     }
-    elsif ( $args->{tag} eq $TAG->{combined_mii}
-	    || $args->{tag} eq $TAG->{single_mii}
-	) {
-	$msg->{status} = $args->{value} eq 'up' ? 'OK' : 'WARN';
+    elsif (    $args->{tag} eq $TAG->{combined_mii}
+            || $args->{tag} eq $TAG->{single_mii} ) {
+        $msg->{status} = $args->{value} eq 'up' ? 'OK' : 'WARN';
     }
-    elsif ( $args->{tag} eq $TAG->{speed}
-	    || $args->{tag} eq $TAG->{duplex}
-	    || $args->{tag} eq $TAG->{hw_address}
-	) {
-	if ( $args->{value} eq 'sides differ' ) {
-	    $msg->{status} = 'WARNING';
-	    $msg->{args} = $args->{rec_meta}{values};
-	}
-	else {
-	    $msg->{status} =  'OK';
-	}
+    elsif (    $args->{tag} eq $TAG->{speed}
+            || $args->{tag} eq $TAG->{duplex}
+            || $args->{tag} eq $TAG->{hw_address} ) {
+        if ( $args->{value} eq 'sides differ' ) {
+            $msg->{status} = 'WARNING';
+            $msg->{args}   = $args->{rec_meta}{values};
+        }
+        else {
+            $msg->{status} = 'OK';
+        }
     }
 
-    $msg->{label}  = $args->{metadata}{file};
-    $msg->{tag}    = $args->{tag};
+    $msg->{label} = $args->{metadata}{file};
+    $msg->{tag}   = $args->{tag};
     $args->{prev_status} ||= $msg->{status};
     $msg->{args} = "link=$args->{metadata}{ident}"
         if exists $args->{metadata}{ident};
-
 
     $self->compare_sides_or_report_record( $args, $msg );
 
@@ -95,148 +91,192 @@ sub eval_discrete_status {
 }
 
 # ----------------------------------------------------------------------
-# 
+# The first line of the bonding file output just identifies the driver,
+# nothing interesting. Ignore this line.
+#
 sub process_header {
     my $self = shift;
     my ( $name, $fh ) = @_;
 
-  HEADER:
+HEADER:
     while ( my $line = <$fh> ) {
-	last HEADER if $line !~ m{\w};
+        last HEADER if $line !~ m{\w};
     }
     return;
 }
 
 # ----------------------------------------------------------------------
-# 
+# The identifier line of the bonding file output is followed by
+# information about the combined link. Extract the currently active
+# slave and the MII status.
+#
+# Bonding Mode: fault-tolerance (active-backup)
+# Primary Slave: sn-link1 (primary_reselect always)
+# Currently Active Slave: sn-link1
+# MII Status: up
+# MII Polling Interval (ms): 100
+# Up Delay (ms): 120000
+# Down Delay (ms): 0
+#    
 sub process_combined_section {
     my $self = shift;
     my ( $name, $fh ) = @_;
 
     my $args = { rec_meta    => {},
-		 prev_status => '',
-		 prev_value  => '',
-		 metadata    => {name => $self->confdata()->{host},
-				 file => $name,
-				 ip => $self->confdata->{ip},
-				 type => $self->confdata->{type},
-		 }};
+                 prev_status => '',
+                 prev_value  => '',
+                 metadata    => {
+                               name => $self->confdata()->{host},
+                               file => $name,
+                               ip   => $self->confdata->{ip},
+                               type => $self->confdata->{type},
+                             } };
 
-  COMBINED:
+COMBINED:
     while ( my $line = <$fh> ) {
-	last COMBINED if $line !~ m{\w};
-	if ( $line =~ m{Currently Active Slave: (\S+)}) {
-	    $args->{value} = $1;
-	    $args->{tag} = $TAG->{current_slave};
-	}
-	elsif ( $line =~ m{MII Status: (\S+)}) {
-	    $args->{value} = $1;
-	    $args->{tag} = $TAG->{combined_mii};
+        last COMBINED if $line !~ m{\w};
+        if ( $line =~ m{Currently Active Slave: (\S+)} ) {
+            $args->{value} = $1;
+            $args->{tag}   = $TAG->{current_slave};
+        }
+        elsif ( $line =~ m{MII Status: (\S+)} ) {
+            $args->{value} = $1;
+            $args->{tag}   = $TAG->{combined_mii};
 
-	}
-	if ( exists $args->{value} ) { # If line has data to report .... 
-	    my ( $status, $newvalue ) = $self->eval_status($args);
-	    $self->prev->{$name}{$TAG->{current_slave}}{value} = $newvalue;
-	    $self->prev->{$name}{$TAG->{current_slave}}{status} = $status;
-	}
+        }
+        if ( exists $args->{value} ) {    # If line has data to report ....
+            my ( $status, $newvalue ) = $self->eval_status($args);
+            $self->prev->{$name}{ $TAG->{current_slave} }{value}  = $newvalue;
+            $self->prev->{$name}{ $TAG->{current_slave} }{status} = $status;
+        }
     }
     return;
 }
+
 # ----------------------------------------------------------------------
-# 
+# The combined section of the bonding file output is followed by
+# detailed information for each of xxx-link1 & xxx-link2. Extract the
+# interface label as the name, then get the MII status, speed and
+# duplex-ity. Finally, once an hour (actually twice, in the same
+# minute), get the Permament HW addr .... Since it's permament, it
+# never changes, but for convenience it's good to have a recent
+# extraction of the value. On the other hand, extracting it twice a
+# minute would be excessive.
+#
+# Besides saving the raw data, also save up and return the key data
+# for one side.
+#
+# Slave Interface: sn-link1
+# MII Status: up
+# Speed: 10000 Mbps
+# Duplex: full
+# Link Failure Count: 7
+# Permanent HW addr: 90:1b:0e:0d:04:4d
+# Slave queue ID: 0
+    
 sub process_section {
     my $self = shift;
     my ( $name, $fh ) = @_;
 
     my $args = { rec_meta    => {},
-		 prev_status => '',
-		 prev_value  => '',
-		 metadata    => {name => $self->confdata()->{host},
-				 file => $name,
-				 ip => $self->confdata->{ip},
-				 type => $self->confdata->{type},
-		 }};
+                 prev_status => '',
+                 prev_value  => '',
+                 metadata    => {
+                               name => $self->confdata()->{host},
+                               file => $name,
+                               ip   => $self->confdata->{ip},
+                               type => $self->confdata->{type},
+                             } };
 
     my $side_info = {};
     my $ident;
-  SECTION_ONE:
+SECTION_ONE:
     while ( my $line = <$fh> ) {
-	last SECTION_ONE if $line !~ m{\w};
-	if ( $line =~ m{Slave Interface: (\S+)}) {
-	    $args->{metadata}->{ident} = $1;
-	    $side_info->{ident} = $1;
-	}
-	elsif ( $line =~ m{MII Status: (\S+)}) {
-	    $args->{value} = $1;
-	    $args->{tag} = $TAG->{single_mii};
-	}
-	elsif ( $line =~ m{Speed: (\S+)\s(\S+)}) {
-	    $args->{value} = $1;
-	    $side_info->{speed} = $1;
-	    $args->{rec_meta}{units} = $2;
-	    $args->{tag} = $TAG->{speed};
-	}
-	elsif ( $line =~ m{Duplex: (\S+)}) {
-	    $args->{value} = $1;
-	    $side_info->{speed} = $1;
-	    $args->{tag} = $TAG->{duplex};
-	}
-	elsif ( $line =~ m{Permanent HW addr: (\S+)}) {
-	    my $tmp = $1;
-	    my ( $sec, $min ) = localtime;
-	    if ( $min == 4 ) {	# report hw addr at start of hour
-		$args->{value} = $tmp;
-		$args->{tag} = $TAG->{hw_address};
-	    }
-	}
-	if ( exists $args->{value} ) { # If line has data to report .... 
-	    $side_info->{$args->{tag}} = $args->{value};
-	    my ( $status, $newvalue ) = $self->eval_status($args);
-	    $self->prev->{$name}{$args->{tag}}{value} = $newvalue;
-	    $self->prev->{$name}{$args->{tag}}{status} = $status;
-	    delete $args->{value};
-	}
+        last SECTION_ONE if $line !~ m{\w};
+        if ( $line =~ m{Slave Interface: (\S+)} ) {
+            $args->{metadata}->{ident} = $1;
+            $side_info->{ident} = $1;
+        }
+        elsif ( $line =~ m{MII Status: (\S+)} ) {
+            $args->{value} = $1;
+            $args->{tag}   = $TAG->{single_mii};
+        }
+        elsif ( $line =~ m{Speed: (\S+)\s(\S+)} ) {
+            $args->{value}           = $1;
+            $side_info->{speed}      = $1;
+            $args->{rec_meta}{units} = $2;
+            $args->{tag}             = $TAG->{speed};
+        }
+        elsif ( $line =~ m{Duplex: (\S+)} ) {
+            $args->{value}      = $1;
+            $side_info->{speed} = $1;
+            $args->{tag}        = $TAG->{duplex};
+        }
+        elsif ( $line =~ m{Permanent HW addr: (\S+)} ) {
+            my $tmp = $1;
+            my ( $sec, $min ) = localtime;
+            if ( $min == 4 ) {    # report hw addr at start of hour
+                $args->{value} = $tmp;
+                $args->{tag}   = $TAG->{hw_address};
+            }
+        }
+        if ( exists $args->{value} ) {    # If line has data to report ....
+            $side_info->{ $args->{tag} } = $args->{value};
+            my ( $status, $newvalue ) = $self->eval_status($args);
+            $self->prev->{$name}{ $args->{tag} }{value}  = $newvalue;
+            $self->prev->{$name}{ $args->{tag} }{status} = $status;
+            delete $args->{value};
+        }
     }
     return $side_info;
 }
+
 # ----------------------------------------------------------------------
+# Compare the MII status, speed and duplex-ity of the two core links.
 #
 sub compare_sides {
     my $self = shift;
     my ( $name, $side_one, $side_two ) = @_;
 
     my $args = { rec_meta    => {},
-		 prev_status => '',
-		 prev_value  => '',
-		 metadata    => {name => $self->confdata()->{host},
-				 file => $name,
-				 ip => $self->confdata->{ip},
-				 type => $self->confdata->{type},
-		 }};
+                 prev_status => '',
+                 prev_value  => '',
+                 metadata    => {
+                               name => $self->confdata()->{host},
+                               file => $name,
+                               ip   => $self->confdata->{ip},
+                               type => $self->confdata->{type},
+                             } };
 
     my @names = ( $side_one->{ident}, $side_two->{ident} );
-  KEY:
+KEY:
     for my $key ( keys %$side_one ) {
-	next KEY if $key eq 'ident';
+        next KEY if $key eq 'ident';
 
-	if ($side_one->{$key} ne $side_two->{$key}) {
-	    $args->{tag} = $key;
-	    $args->{value} = 'sides differ';
-	    $args->{rec_meta}{values}
-	    = "$names[0]=$side_one->{$key};$names[1]=$side_two->{$key}";
-	    my ( $status, $newvalue ) = $self->eval_status($args);	    
-	}
+        if ( $side_one->{$key} ne $side_two->{$key} ) {
+            $args->{tag}   = $key;
+            $args->{value} = 'sides differ';
+            $args->{rec_meta}{values}
+                = "$names[0]=$side_one->{$key};$names[1]=$side_two->{$key}";
+            my ( $status, $newvalue ) = $self->eval_status($args);
+        }
     }
     return;
 }
+
 # ----------------------------------------------------------------------
-# 
+# Parse the data returned from a single bond file.
+#
+# Open the file, skip the header, parse the combined section, parse
+# and save the two individual link sections. Compare the extracted
+# data.
+#
 sub parse_bond_status {
     my $self = shift;
-    my ( $file ) = @_;
+    my ($file) = @_;
 
     open my $fh, '<', $file
-	or die "COuld not open file '$file': $!.\n";
+        or die "COuld not open file '$file': $!.\n";
 
     my $name = basename $file;
     $self->process_header( $name, $fh );
@@ -246,19 +286,24 @@ sub parse_bond_status {
     $self->compare_sides( $name, $side_one, $side_two );
     return;
 }
+
 # ----------------------------------------------------------------------
-# Top-level program, invoked from loop_core().
+# Top-level program, invoked from loop_core(). 
+#
+# Compare the return values of all bonding files in the /proc/net/bonding/
+# directory ( specified in config file ).
 #
 sub query_target {
     my $self = shift;
 
-    state $files  = [glob( $self->confdata->{dir} . '/*-bond1' )];
-    
-    for my $file ( @$files ) {
-	$self->parse_bond_status( $file );
+    state $files = [ glob( $self->confdata->{dir} . '/*-bond1' ) ];
+
+    for my $file (@$files) {
+        $self->parse_bond_status($file);
     }
     return;
 }
+
 # ======================================================================
 1;
 __END__
@@ -268,7 +313,7 @@ __END__
 
 =head1 NAME
 
-     AN::RAID::Temperature.pm - package to handle RAID temperature values
+     AN::Bonding.pm - package to handle Ethernet Channel Bonding.
 
 =head1 VERSION
 
@@ -276,14 +321,15 @@ This document describes AN::RAID::Temperature.pm version 1.0.0
 
 =head1 SYNOPSIS
 
-    use AN::RAID::Temperature;
-    my $agent = AN::RAID::Temperature->new( );
+    use AN::Bonding
+    my $agent = AN::Bonding->new( );
     $agent->run();
 
 =head1 DESCRIPTION
 
-This module implements the AN::RAID::Temperature class which runs an agent
-to query RAID Controllers using the storcli program.
+This module implements the AN::Bonding class which runs an agent to
+query the Ethernet Channel Bonding driver for each of the three
+networks: internet-facing, back-channel, and storage.
 
 =head1 METHODS
 
@@ -300,6 +346,10 @@ Provide fast constants.
 =item B<Data::Dumper> I<core>
 
 Display data structures in debug messages.
+
+=item B<English> I<core>
+
+Provides meaningful names for Perl 'punctuation' variables.
 
 =item B<version> I<core>
 
@@ -348,8 +398,7 @@ There are no current incompatabilities.
 
 Alteeve's Niche!  -  https://alteeve.ca
 
-Tom Legrady       -  tom@alteeve.ca	November 2014
-
+Tom Legrady       -  tom@alteeve.ca	February 2015
 =cut
 
 # End of File
