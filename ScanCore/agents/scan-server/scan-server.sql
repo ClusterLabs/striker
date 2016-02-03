6e090ee1c70b30b9150e529ccbade1dc3fbb2476
Digimer <digimer@alteeve.ca>
2016-02-03 00:53:28 -0500

    * Continuing work on 'scan-server'... It's far from done.
    Signed-off-by: Digimer <digimer@alteeve.ca>

--- ScanCore/agents/scan-server/scan-server.sql
+++ ScanCore/agents/scan-server/scan-server.sql
@@ -3,173 +3,115 @@
 --     - The history schema must have a column called 'history_id' which is a 'bigserial' as well.
 --     - There must be a column called 'X_host_uuid' that is a foreign key to  'hosts -> host_uuid'.
 --     
+-- Servers are distinct entities that can exist on either node or move entirely to another Anvil!. Users
+-- must be aware of this and account for a server moving away and coming back over time. This can be 
+-- accounted for, in part, but having the agent scan 'virsh --connect qemu+ssh://root@<peer>/system ...'. The
+-- 'server_host_uuid', then, simply indicates which host/node gathered the data.
+-- 
 
--- This is the database schema for the 'Clustat' scan agent.
-
-CREATE TABLE clustat (
-	clustat_uuid			uuid				primary key,
-	clustat_host_uuid		uuid				not null,
-	clustat_quorate			boolean				not null,			-- Is this node quorate?
-	clustat_cluster_name		text				not null,			-- the cluster name reported by clustat.
-	modified_date			timestamp with time zone	not null,
-	
-	FOREIGN KEY(clustat_host_uuid) REFERENCES hosts(host_uuid)
+-- This stores information about the server.
+CREATE TABLE server (
+	server_uuid		uuid				not null	primary key,	-- This comes from the server's XML definition file.
+	server_host_uuid	uuid				not null,
+	server_name		text				not null,
+	server_stop_reason	text,								-- Set by Striker to 'clean' when stopped via the webui. This prevents anvil-safe-start from starting it on node boot.
+	server_start_group	integer				not null	default 1,
+	server_start_delay	integer				not null	default 0,	-- How many seconds to delay booting for after the last server in the previous group boots.
+	modified_date		timestamp with time zone	not null
 );
-ALTER TABLE clustat OWNER TO #!variable!user!#;
+ALTER TABLE server OWNER TO #!variable!user!#;
 
-CREATE TABLE history.clustat (
-	history_id			bigserial			not null,
-	clustat_uuid			uuid				not null,
-	clustat_host_uuid		uuid				not null,
-	clustat_quorate			boolean				not null,
-	clustat_cluster_name		text				not null,
-	modified_date			timestamp with time zone	not null
+CREATE TABLE history.server (
+	history_id		bigserial,
+	server_uuid		uuid,
+	server_host_uuid	uuid,
+	server_name		text,
+	server_stop_reason	text,
+	server_start_group	integer,
+	server_start_delay	integer,
+	modified_date		timestamp with time zone	not null
 );
-ALTER TABLE history.clustat OWNER TO #!variable!user!#;
+ALTER TABLE history.server OWNER TO #!variable!user!#;
 
-CREATE FUNCTION history_clustat() RETURNS trigger
+CREATE FUNCTION history_server() RETURNS trigger
 AS $$
 DECLARE
-	history_clustat RECORD;
+	history_server RECORD;
 BEGIN
-	SELECT INTO history_clustat * FROM clustat WHERE clustat_uuid=new.clustat_uuid;
-	INSERT INTO history.clustat
-		(clustat_uuid,
-		 clustat_host_uuid,
-		 clustat_quorate,
-		 clustat_cluster_name,
+	SELECT INTO history_server * FROM server WHERE server_uuid = new.server_uuid;
+	INSERT INTO history.server
+		(server_uuid,
+		 server_host_uuid, 
+		 server_name, 
+		 server_stop_reason, 
+		 server_start_group, 
+		 server_start_delay, 
 		 modified_date)
 	VALUES
-		(history_clustat.clustat_uuid,
-		 history_clustat.clustat_host_uuid,
-		 history_clustat.clustat_quorate,
-		 history_clustat.clustat_cluster_name,
-		 history_clustat.modified_date);
+		(history_server.server_uuid, 
+		 history_server.server_host_uuid, 
+		 history_server.server_name, 
+		 history_server.server_stop_reason, 
+		 history_server.server_start_group, 
+		 history_server.server_start_delay, 
+		 history_server.modified_date);
 	RETURN NULL;
 END;
 $$
 LANGUAGE plpgsql;
-ALTER FUNCTION history_clustat() OWNER TO #!variable!user!#;
+ALTER FUNCTION history_server() OWNER TO #!variable!user!#;
 
-CREATE TRIGGER trigger_clustat
-	AFTER INSERT OR UPDATE ON clustat
-	FOR EACH ROW EXECUTE PROCEDURE history_clustat();
+CREATE TRIGGER trigger_server
+	AFTER INSERT OR UPDATE ON server
+	FOR EACH ROW EXECUTE PROCEDURE history_server();
 
 
+-- This stores extended, rarely changing data about the server
+CREATE TABLE server_data (
+	server_data_uuid	uuid				not null	primary key,	-- This comes from the server's XML definition file.
+	server_data_server_uuid	uuid				not null,
+	server_data_note	text				not null,			-- User-setable note section.
+	server_data_xml		text,								-- Contains the master copy of the server's XML file.
+	modified_date		timestamp with time zone	not null, 
 	
--- This is where information on nodes, as reported by clustat, are stored.
-CREATE TABLE clustat_node (
-	clustat_node_name		text				not null,			-- Node name (from the 'Member Name' column)
-	clustat_node_id			bigserial			not null,
-	clustat_node_clustat_uuid	uuid				not null,
-	clustat_node_cluster_id		bigint				not null,			-- This is the node ID reported by clustat
-	clustat_node_status		text				not null,			-- Node status
-	modified_date			timestamp with time zone	not null,
-	
-	FOREIGN KEY(clustat_node_clustat_uuid) REFERENCES clustat(clustat_uuid)
+	FOREIGN KEY(server_data_server_uuid) REFERENCES server(server_uuid)
 );
-ALTER TABLE clustat_node OWNER TO #!variable!user!#;
+ALTER TABLE server_data OWNER TO #!variable!user!#;
 
-CREATE TABLE history.clustat_node (
-	history_id			bigserial			not null,
-	clustat_node_id			bigint				not null,
-	clustat_node_clustat_uuid	uuid				not null,
-	clustat_node_cluster_id		bigint				not null,
-	clustat_node_name		text				not null,
-	clustat_node_status		text				not null,
-	modified_date			timestamp with time zone	not null
+CREATE TABLE history.server_data (
+	history_id		bigserial,
+	server_data_uuid	uuid,
+	server_data_server_uuid	uuid,
+	server_data_note	text,
+	server_data_xml		text,
+	modified_date		timestamp with time zone	not null
 );
-ALTER TABLE history.clustat_node OWNER TO #!variable!user!#;
+ALTER TABLE history.server_data OWNER TO #!variable!user!#;
 
-CREATE FUNCTION history_clustat_node() RETURNS trigger
+CREATE FUNCTION history_server_data() RETURNS trigger
 AS $$
 DECLARE
-	history_clustat_node RECORD;
+	history_server_data RECORD;
 BEGIN
-	SELECT INTO history_clustat_node * FROM clustat_node WHERE clustat_node_id=new.clustat_node_id;
-	INSERT INTO history.clustat_node
-		(clustat_node_id,
-		 clustat_node_clustat_uuid,
-		 clustat_node_cluster_id,
-		 clustat_node_name,
-		 clustat_node_status,
+	SELECT INTO history_server_data * FROM server_data WHERE server_data_uuid = new.server_data_uuid;
+	INSERT INTO history.server_data
+		(server_data_uuid,
+		 server_data_server_uuid, 
+		 server_data_note, 
+		 server_data_xml, 
 		 modified_date)
 	VALUES
-		(history_clustat_node.clustat_node_id,
-		 history_clustat_node.clustat_node_clustat_uuid,
-		 history_clustat_node.clustat_node_cluster_id,
-		 history_clustat_node.clustat_node_name,
-		 history_clustat_node.clustat_node_status,
-		 history_clustat_node.modified_date);
+		(history_server_data.server_data_uuid, 
+		 history_server_data.server_data_server_uuid, 
+		 history_server_data.server_data_note, 
+		 history_server_data.server_data_xml, 
+		 history_server_data.modified_date);
 	RETURN NULL;
 END;
 $$
 LANGUAGE plpgsql;
-ALTER FUNCTION history_clustat_node() OWNER TO #!variable!user!#;
+ALTER FUNCTION history_server_data() OWNER TO #!variable!user!#;
 
-CREATE TRIGGER trigger_clustat_node
-	AFTER INSERT OR UPDATE ON clustat_node
-	FOR EACH ROW EXECUTE PROCEDURE history_clustat_node();
-	
-	
--- This stores information about clustat services.
-CREATE TABLE clustat_service (
-	clustat_service_id		bigserial			not null,
-	clustat_service_clustat_uuid	uuid				not null,
-	clustat_service_name		text				not null,
-	clustat_service_host		text,
-	clustat_service_status		text,
-	clustat_service_notes		text,
-	clustat_service_is_vm		boolean				not null,
-	modified_date			timestamp with time zone	not null,
-	
-	FOREIGN KEY(clustat_service_clustat_uuid) REFERENCES clustat(clustat_uuid)
-);
-ALTER TABLE clustat_service OWNER TO #!variable!user!#;
-
-CREATE TABLE history.clustat_service (
-	history_id			bigserial			not null,
-	clustat_service_id		bigint				not null,
-	clustat_service_clustat_uuid	uuid				not null,
-	clustat_service_name		text				not null,
-	clustat_service_host		text,
-	clustat_service_status		text,
-	clustat_service_notes		text,
-	clustat_service_is_vm		boolean				not null,
-	modified_date			timestamp with time zone	not null
-);
-ALTER TABLE history.clustat_service OWNER TO #!variable!user!#;
-
-CREATE FUNCTION history_clustat_service() RETURNS trigger
-AS $$
-DECLARE
-	history_clustat_service RECORD;
-BEGIN
-	SELECT INTO history_clustat_service * FROM clustat_service WHERE clustat_service_id=new.clustat_service_id;
-	INSERT INTO history.clustat_service
-		(clustat_service_id,
-		 clustat_service_clustat_uuid,
-		 clustat_service_name,
-		 clustat_service_host,
-		 clustat_service_status,
-		 clustat_service_notes, 
-		 clustat_service_is_vm,
-		 modified_date)
-	VALUES
-		(history_clustat_service.clustat_service_id,
-		 history_clustat_service.clustat_service_clustat_uuid,
-		 history_clustat_service.clustat_service_name,
-		 history_clustat_service.clustat_service_host,
-		 history_clustat_service.clustat_service_status,
-		 history_clustat_service.clustat_service_notes, 
-		 history_clustat_service.clustat_service_is_vm,
-		 history_clustat_service.modified_date);
-	RETURN NULL;
-END;
-$$
-LANGUAGE plpgsql;
-ALTER FUNCTION history_clustat_service() OWNER TO #!variable!user!#;
-
-CREATE TRIGGER trigger_clustat_service
-	AFTER INSERT OR UPDATE ON clustat_service
-	FOR EACH ROW EXECUTE PROCEDURE history_clustat_service();
+CREATE TRIGGER trigger_server_data
+	AFTER INSERT OR UPDATE ON server_data
+	FOR EACH ROW EXECUTE PROCEDURE history_server_data();
