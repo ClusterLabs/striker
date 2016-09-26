1efb85daabd5407b05f94facf7f6f885df7f7971
Digimer <digimer@alteeve.ca>
2016-09-26 14:42:10 -0400

    * Fixed a bug where Cold Stop wasn't working because 'System->poweroff()', when called locally, wasn't printing the host name of the machine being shut down. * Fixed a 'storge' typo.
    Signed-off-by: Digimer <digimer@alteeve.ca>

--- AN/Tools/System.pm
+++ AN/Tools/System.pm
@@ -477,7 +477,7 @@ sub poweroff
 	my $target   = $parameter->{target}   ? $parameter->{target}   : "";
 	my $port     = $parameter->{port}     ? $parameter->{port}     : "";
 	my $password = $parameter->{password} ? $parameter->{password} : "";
-	$an->Log->entry({log_level => 3, message_key => "an_variables_0002", message_variables => {
+	$an->Log->entry({log_level => 2, message_key => "an_variables_0002", message_variables => {
 		name1 => "target", value1 => $target, 
 		name2 => "port",   value2 => $port, 
 	}, file => $THIS_FILE, line => __LINE__});
@@ -498,7 +498,7 @@ sub poweroff
 		print "poweroff: $target\n";
 		
 		### Remote calls
-		$an->Log->entry({log_level => 3, message_key => "an_variables_0002", message_variables => {
+		$an->Log->entry({log_level => 2, message_key => "an_variables_0002", message_variables => {
 			name1 => "shell_call", value1 => $shell_call,
 			name2 => "target",     value2 => $target,
 		}, file => $THIS_FILE, line => __LINE__});
@@ -513,10 +513,10 @@ sub poweroff
 	else
 	{
 		# This is an untranslated string used by '$an->Striker->_cold_stop_anvil()' to know which machine is powering down.
-		print "poweroff: $target\n";
+		print "poweroff: ".$an->hostname."\n";
 		
 		### Local calls
-		$an->Log->entry({log_level => 3, message_key => "an_variables_0001", message_variables => {
+		$an->Log->entry({log_level => 2, message_key => "an_variables_0001", message_variables => {
 			name1 => "shell_call", value1 => $shell_call, 
 		}, file => $THIS_FILE, line => __LINE__});
 		open (my $file_handle, "$shell_call 2>&1 |") or $an->Alert->error({title_key => "error_title_0020", message_key => "error_message_0022", message_variables => { shell_call => $shell_call, error => $! }, code => 30, file => $THIS_FILE, line => __LINE__});
